85949b1d39242b665125e0a5b34dae241fde64bd
Caleb Wagner <calebwagner77@gmail.com>
2024-01-30 13:25:43 -0500

    moved location of DTO folder and excluded old folder from project

---
+++ JwtAuthLogin/Core/Dtos/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthLogin.Core.DbContext.Dtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+
+    }
+}
